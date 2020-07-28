c6928a8b43502eb719e0d32dca6c208eb8bcb0c9
jevans7720 <jevans7720@icloud.com>
2020-07-28 18:48:21 -0400

    Compile_html.js * added code to emulating the roll20 post processing done to html for local development * added options for command line use * added help text * cannot emulate roll20 js at this time PF2E.scss * made changes to color and borders to create more uniform bordering using css grid * added a left and right global classes, not sure if more of these will be useful yet, these ones most likely won't be useful long term. top.scss and nav.scss * added some grid related stuff for layout visibility while I work on combat Assets folder * added blood and shield images combat-col.html * wrote HTML for HP section * duplicated HP HTML for shield section, WIP combat-col.scss * Wrote some reusable css for all items in the combat column * Added css for all items above the action/spells box * Wrote css for HP and some for armor Also added the output files from the compile script for tracking May add a --prod option to the compile script to generate final files with changes and change the default behavor to generate [filename]_[timestamp].html/css files the won't be tracked

--- Pathfinder 2E by PAUR/compile_html.js
+++ Pathfinder 2E by PAUR/compile_html.js
@@ -1,9 +1,53 @@
+//includes
 let nunjucks = require('nunjucks');
 let sass = require('node-sass');
 let fs = require('fs');
+// env variables
+let outHTML = "pathfinder2_gag.html";
+let outCSS = "pathfinder2_gag.css";
+let local = false;
+let args = process.argv.slice(2);
+const classRegexString = /(?<=class="[0-z\- _]*)\b(?!(attr_|roll_|repeating_|(?<!( |\")))[0-z\-]*)[0-z\-]+/gm;
+const assetRegex = /img src=\"assets\//gm;
+function classReplaceFunction(match){
+  console.log(match);
+  return `sheet-${match}`;
+}
+//We'll do something with this later
+for(let arg of args){
+  //console.log(arg);
+  switch(arg){
+    case "-?":
+    case "-h":
+    case "--help":
+      console.log(`Usage: node compile_html.js [OPTION]
+Compiles nunjucks and SASS into HTML and CSS for Roll20.
+
+  -l, --local       Compile the code for local testing
+                    Adds additional HTML and Roll20 like post processing.
+  -h, --help        Displays this help text.
+  -?                Alias for --help`);
+      return;
+    case "-l":
+    case "--local":
+      local = true;
+      outHTML = "pathfinder2_gag_local.html";
+      outCSS = "pathfinder2_gag_local.css";
+      break;
+    default:
+      console.log(`Unrecognized flag "${arg}"`);
+  }
+
+}
+
+//some regec I'm working on
+//(?<=class="[0-z\- _]*)\b(?!(attr_|roll_|repeating_|(?<!( |\")))[0-z\-]*)[0-z\-]+
 
 //nunjucks compile
 let html = '';
+if (local){
+  html+="<!DOCTYPE html><html><head><link href=\"pathfinder2_gag_local.css\" rel=\"stylesheet\"></head><body>";
+}
 //nunjucks configuration. documentation found here https://mozilla.github.io/nunjucks/api.html
 nunjucks.configure({
     autoexcape: true,
@@ -11,10 +55,17 @@ nunjucks.configure({
     lstripBlocks: true,
     //may include watch:true later if I feel like putting more effort into this so.
 });
-html = nunjucks.render('src/PF2E.html');
+html += nunjucks.render('src/PF2E.html');
+if (local){
+  html+="</body></html>";
+  html=html.replace(classRegexString, classReplaceFunction);
+}
+else{
+  html=html.replace(assetRegex, "img src=\"http://www.broadsidehd.com/paur/");
+}
 
 //reqork this to shwo erros maybe? someday...
-fs.writeFile('pathfinder2_gag.html', html,()=>{console.log('HTML compile complete.')});
+fs.writeFile(outHTML, html,()=>{console.log('HTML compile complete.')});
 
 //sass compile
 let css = '';
@@ -23,4 +74,4 @@ css = sass.renderSync({
     file: "src/PF2E.scss"
   }).css;
   console.log(css);
-fs.writeFile('pathfinder2_gag.css', css,()=>{console.log('SASS compile complete.')});
+fs.writeFile(outCSS, css,()=>{console.log('SASS compile complete.')});
